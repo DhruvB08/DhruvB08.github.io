8ab428818def17cb72544c8ab4b11a24854ec530
Drew Drew <DhruvB08@Gmail.com>
2018-05-20 01:08:26 -0400

    major changes

--- site/assets/globals.js
+++ site/assets/globals.js
@@ -1,6 +1,6 @@
-$(function() {
+function initialize() {
 	document.getElementById('home').innerHTML = 'Home';
 	document.getElementById('projects').innerHTML = 'Projects';
 	document.getElementById('contactMe').innerHTML = 'Contact Me';
 	document.getElementById('resume').innerHTML = 'Resume';
-});
+}
