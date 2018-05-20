ceba4f7922846cac5ea1c583a7e5e84887b3fcd8
Drew Drew <DhruvB08@Gmail.com>
2018-05-20 00:51:45 -0400

    more index changes

--- site/assets/globals.js
+++ site/assets/globals.js
@@ -1,4 +1,4 @@
-document.getElementById('home').innerHTML = "Home";
-document.getElementById('projects').innerHTML = "Projects";
-document.getElementById('contactMe').innerHTML = "Contact Me";
-document.getElementById('resume').innerHTML = "Resume";
+document.getElementById('home').innerHTML = 'Home';
+document.getElementById('projects').innerHTML = 'Projects';
+document.getElementById('contactMe').innerHTML = 'Contact Me';
+document.getElementById('resume').innerHTML = 'Resume';
