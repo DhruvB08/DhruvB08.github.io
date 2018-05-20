496cadd9dc4afa9b8f0d96acbbe51daa8dbe72af
Drew Drew <DhruvB08@Gmail.com>
2018-05-20 00:13:58 -0400

    major changes

---
+++ site/assets/globals.js
@@ -0,0 +1,5 @@
+document.getElementsByClassName("home").innerHTML = "Home";
+document.getElementsByClassName("projects").innerHTML = "Projects";
+document.getElementsByClassName("contactMe").innerHTML = "Contact Me";
+document.getElementsByClassName("resume").innerHTML = "Resume";
+
