02f6383c3f04ee4060cd6024d62ac130e300d52f
Drew Drew <DhruvB08@Gmail.com>
2018-05-20 01:56:19 -0400

    updated projects tab

--- site/assets/globals.js
+++ site/assets/globals.js
@@ -1,9 +1,21 @@
 function initialize() {
-	document.getElementById('home').innerHTML = 'Home';
+	document.getElementById('home').innerHTML = 'About Me';
 	document.getElementById('projects').innerHTML = 'Projects';
 	document.getElementById('contactMe').innerHTML = 'Contact Me';
 	document.getElementById('resume').innerHTML = 'Resume';
 
 
 	document.getElementById('shortBio').innerHTML = 'I\'m a college student, recently graduated from Rutgers University, with a B.S. in Computer Science and a minor in Economics, both done during the four years I spent there. I\'m originally from India, but then moved to the United States when I was just 2 years old. For a few years my family stayed in Edison, then after the end of my 3rd grade, we moved to East Brunswick. From 4th grade to 12th grade I was living in East Brunswick. During college I\'ve been bouncing back and forth between New Brunswick and East Brunswick, and I\'d like to consider both places my home really.';
+
+	document.getElementById('proj1LinkName').innerHTML = 'Dhruv Bhardwaj';
+	document.getElementById('proj1Title').innerHTML = 'My Personal Website';
+	document.getElementById('proj1Descript').innerHTML = 'This is my own personal website I made in a few hours as a side project. I decided it\'d be a good thing to do for the experience, and to put some of the languages and skills I\'ve picked up into practice. I will be going back to this and adding updates and changes every time I\'m free and learn something new, or have more ideas for it.';
+
+	document.getElementById('proj2LinkName').innerHTML = 'www.ruphisigmakappa.com';
+	document.getElementById('proj2Title').innerHTML = 'Phi Sigma Kappa Fraternity Website';
+	document.getElementById('proj2Descript').innerHTML = 'Created by four of us, all CS majors and brothers here at the Rutgers chapter of Phi Sigma Kappa. It\'s updated every semester now, and each of us adds our own changes and improvements whenever we want to work on it.';
+
+	document.getElementById('proj3LinkName').innerHTML = 'RUPhiSigTest.Github.IO';
+	document.getElementById('proj3Title').innerHTML = 'My take on the PSK website';
+	document.getElementById('proj3Descript').innerHTML = 'This is my own version of the Rutgers PSK website, created 100% by me. This was a side project I wanted to make entirely on my own so I know every step of the process and can replicate it as needed. I also added a few minor design changes, making it look generally better in my opinion';
 }
