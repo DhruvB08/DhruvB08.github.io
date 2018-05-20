e56561c84ddabceff4622db9278c754bdd9610ef
Drew Drew <DhruvB08@Gmail.com>
2018-05-20 01:42:22 -0400

    added selfie

--- site/assets/globals.js
+++ site/assets/globals.js
@@ -3,4 +3,7 @@ function initialize() {
 	document.getElementById('projects').innerHTML = 'Projects';
 	document.getElementById('contactMe').innerHTML = 'Contact Me';
 	document.getElementById('resume').innerHTML = 'Resume';
+
+
+	document.getElementById('shortBio').innerHTML = 'I\'m a college student, recently graduated from Rutgers University, with a B.S. in Computer Science and a minor in Economics, both done during the four years I spent there. I\'m originally from India, but then moved to the United States when I was just 2 years old. For a few years my family stayed in Edison, then after the end of my 3rd grade, we moved to East Brunswick. From 4th grade to 12th grade I was living in East Brunswick. During college I\'ve been bouncing back and forth between New Brunswick and East Brunswick, and I\'d like to consider both places my home really.';
 }
